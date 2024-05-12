b1bde8eb5552f25901126a8cdd24b78426490cd5
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-12 10:25:54 -0400

    experiments

--- readme.md
+++ readme.md
@@ -11,3 +11,12 @@ WGS84 bounding box coordinates.
 dotnet run
 
 ```
+
+# Misc Experiments
+
+```bash
+gdalwarp -overwrite -to SRC_METHOD=NO_GEOTRANSFORM -te 0.0 0.0 2048.0 2048.0 /vsicurl/http://download.osgeo.org/pub/gdal/data/nitf/u_3054a.ntf /tmp/out.tif
+
+
+```
+
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
+using OSGeo.GDAL;
 
 public class Program {
   public static async Task Main(string[] args) {
-    Console.WriteLine("Hello c#!");
+    if (args.Length < 3) {
+      Console.WriteLine("Usage: NITF_Ranger.exe http://host.name/path/to/file.ntf '[minX, maxX, minY, maxY]' ./path/to/output.tiff");
+      return;
+    }
+    string file_url = args[0];
+    string bbox_str = args[1];
+    string output_file = args[2];
+
+    Console.WriteLine($"Reading metadata from {file_url}");
+
+    try {
+      Gdal.AllRegister();
+    }
+    catch (Exception ex) {
+      Console.WriteLine(""+ex);
+    }
+
+    Dataset ds = Gdal.Open("/vsicurl/"+file_url, Access.GA_ReadOnly );
+    Console.WriteLine("ds = "+ds);
+
+
 
   }
 }
