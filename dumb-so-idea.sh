41368c3b73c4c686c280c952e069e30c0e558f74
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-13 20:51:03 -0400

    nonsense

---
+++ dumb-so-idea.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p data
+
+X1=0
+Y1=0
+X2=32
+Y2=32
+Z=9
+for x in `seq $X1 $X2`; do
+  for y in `seq $Y1 $Y2`; do
+    if ! [[ -e "data/${Z}_${y}_${x}.png" ]] ; then
+      echo "Getting ${x},${y}"
+      curl -s "https://tile.openstreetmap.org/${Z}/${x}/${y}.png" -o "data/${Z}_${y}_${x}.png"
+    else
+      echo "Already-exists ${x},${y}"
+    fi
+  done
+done
+
+montage -mode concatenate -tile "$((X2-X1+1))x" data/"${Z}_*.png" "data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png"
+
+echo "See data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png "
