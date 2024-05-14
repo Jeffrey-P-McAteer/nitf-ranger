4a5679bd2ed22e2be4e74056046050a131df5bd6
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-14 19:09:33 -0400

    stuff

--- dumb-so-idea.sh
+++ dumb-so-idea.sh
@@ -6,9 +6,9 @@ mkdir -p data
 
 X1=0
 Y1=0
-X2=32
-Y2=32
-Z=9
+X2=30
+Y2=30
+Z=5
 for x in `seq $X1 $X2`; do
   for y in `seq $Y1 $Y2`; do
     if ! [[ -e "data/${Z}_${y}_${x}.png" ]] ; then
@@ -23,3 +23,10 @@ done
 montage -mode concatenate -tile "$((X2-X1+1))x" data/"${Z}_*.png" "data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png"
 
 echo "See data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png "
+
+ls -alh "data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png"
+
+feh "data/out_z${Z}_${X1}_${Y1}-${X2}_${Y2}.png"
+
+
+
