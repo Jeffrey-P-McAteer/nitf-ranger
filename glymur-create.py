a662e96cda85ffdfcf73f4dc3e5676ca95234d3a
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-19 11:30:06 -0400

    jp header parsing

--- glymur-create.py
+++ glymur-create.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import subprocess
+import struct
 
 pyenv = os.path.join(os.path.dirname(__file__), 'pyenv')
 os.makedirs(pyenv, exist_ok=True)
@@ -46,14 +47,14 @@ except:
 
 import skimage.data
 
-print(f'glymur = {glymur}')
-j2kfile = glymur.data.goodstuff()
-print(f'j2kfile = {j2kfile}')
-j2k = glymur.Jp2k(j2kfile)
-print(f'j2k = {j2k}')
+#print(f'glymur = {glymur}')
+#j2kfile = glymur.data.goodstuff()
+#print(f'j2kfile = {j2kfile}')
+#j2k = glymur.Jp2k(j2kfile)
+#print(f'j2k = {j2k}')
 
 astronaut = skimage.data.astronaut()
-print(f'astronaut = {astronaut}')
+# print(f'astronaut = {astronaut}')
 x_count = 4
 y_count = 4
 
@@ -75,7 +76,7 @@ if not os.path.exists('astronaut.jp2'):
       for col_i, col_val in enumerate(row):
         final_img[row_i + (i * len(astronaut)) ][col_i + (i * len(astronaut[0])) ] = astronaut[row_i][col_i]
 
-  print(f'final_img = {len(final_img)}x{len(final_img[0])}')
+  print(f'final_img size = {len(final_img)}x{len(final_img[0])}')
 
   #jp2[:] = astronaut
   jp2[:] = final_img
@@ -91,10 +92,55 @@ y1 = int( ((y_count * len(astronaut)) / 2.0) + (len(astronaut) / 2.0) )
 w = int(x1 - x0)
 h = int(y1 - y0)
 
+print(f'Reading the rectangle from {x0},{y0} -> {x1},{y1} ({w}x{h} pixels)')
+
 out_pixels = numpy.zeros((w, h, 3), dtype=numpy.uint8)
 with open('astronaut.jp2', 'rb') as fd:
-  pass
+  jp2_file_len = os.fstat(fd.fileno()).st_size
+  # Ref: https://www.file-recovery.com/jp2-signature-format.htm
+
+  # Read first chunk length (big-endian, 4 bytes)
+  fd.seek(0)
+  c1_len = struct.unpack('>I', fd.read(4))[0]
+  c1_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+  print(f'c1_len = {c1_len}, c1_type = {c1_type}')
+
+  # Seek past any remaining bytes in chunk 1; seek's argument is ALWAYS offset from pos 0
+  fd.seek(c1_len)
+  c2_len = struct.unpack('>I', fd.read(4))[0]
+  c2_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+  print(f'c2_len = {c2_len}, c2_type = {c2_type}')
+
+  if c2_type == 'ftyp':
+    # Parse sub-type at offset 8
+    c2_sub_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+    print(f'c2_sub_type = {c2_sub_type}')
 
 
-matplotlib.pyplot.imshow(out_pixels, interpolation='nearest')
-matplotlib.pyplot.show()
+  fd.seek(c1_len + c2_len)
+  c3_len = struct.unpack('>I', fd.read(4))[0]
+  c3_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+  print(f'c3_len = {c3_len}, c3_type = {c3_type}')
+
+  fd.seek(c1_len + c2_len + c3_len)
+  c4_len = struct.unpack('>I', fd.read(4))[0]
+  c4_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+  print(f'c4_len = {c4_len}, c4_type = {c4_type}')
+
+
+
+  fd.seek(c1_len + c2_len + c3_len + c4_len)
+  if fd.tell() < jp2_file_len:
+    c5_len = struct.unpack('>I', fd.read(4))[0]
+    c5_type = b''.join(struct.unpack('cccc', fd.read(4))).decode('utf-8')
+    print(f'c5_len = {c5_len}, c5_type = {c5_type}')
+
+
+
+if 'display' in sys.argv:
+  matplotlib.pyplot.imshow(out_pixels, interpolation='nearest')
+  matplotlib.pyplot.show()
+else:
+  print('pass "display" as arg to show de-coded bounding box pixels using pyplot')
+
+
